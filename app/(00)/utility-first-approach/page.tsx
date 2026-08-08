651bdf92f1b2ef15d74316745b59340e765ff7a9
kalan <indulakalana@gmail.com>
2026-08-08 16:17:36 +0530

    utitlity first apporach

---
+++ app/(00)/utility-first-approach/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Page = () => {
+    return (
+        <div>
+
+            <button className="p-3 bg-blue-600 rounded-md text-white">Get Started</button>
+
+        </div>
+    )
+}
+export default Page
